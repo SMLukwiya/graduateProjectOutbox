830ab93598d17b61d6fd559ab992839b3c4ee206
SMLukwiya <sundaymorganl@gmail.com>
2019-06-09 11:27:03 +0300

    model for house for sale

---
+++ server/models/buy.model.js
@@ -0,0 +1,45 @@
+import mongoose from 'mongoose';
+import crypto from 'crypto';
+
+
+const ForSaleSchema = new mongoose.Schema(
+    {
+    name: {
+      type: String,
+      trim:true,
+      required: "Name is required"
+    },
+    about: {
+      type: String,
+      trim: true,
+      required: "description is required"
+    },
+    image: {
+        data: Buffer,
+        contentType: String
+      },
+    price: {
+      type: Number,
+      required: 'Price is required'
+    },
+    catergory: {
+      type: String,
+      trim: true,
+      required: "Is it for sale or rent?"
+    },
+    location: {
+      type: String,
+      trim: true,
+      unique: "Location already exists",
+      required: "Location is required"
+    },
+    created: {
+      type: Date,
+      default: Date.now
+    },
+    updated: Date
+  });
+
+
+
+export default mongoose.model('ForSale', ForSaleSchema);
