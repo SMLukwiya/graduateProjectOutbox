f2457a67440e2f771c8d4aeeb134f5b7ee94951a
SMLukwiya <sundaymorganl@gmail.com>
2019-06-09 10:23:00 +0300

    template entry point for jsx code

---
+++ template.js
@@ -0,0 +1,17 @@
+export default () => {
+    return (`<!doctype html>
+      <html lang="en">
+        <head>
+          <meta charset="utf-8">
+          <title>BUYRENT</title>
+          <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500&display=swap" />
+          <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+          <style id="jss-server-side"></style>
+        </head>
+        <body>
+          <div id="root"></div>
+          <script type="text/javascript" src="/dist/bundle.js"></script>
+          <style id="jss-server-side"></style>
+        </body>
+      </html>`)
+}
