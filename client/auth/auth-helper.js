1c47e1fa9a8c10940647c77db31cc3a4fc614c3a
SMLukwiya <sundaymorganl@gmail.com>
2019-06-09 12:06:11 +0300

    client side authentication helper function

---
+++ client/auth/auth-helper.js
@@ -0,0 +1,31 @@
+const auth = {
+//Retrieve credentials if signed-in.
+  isAuthenticated() {
+    if (typeof window == "undefined") {
+      return false;
+    }
+
+    if (sessionStorage.getItem('jwt')) {
+      return JSON.parse(sessionStorage.getItem('jwt'));
+    }
+    else {
+      return false;
+    }
+  },
+//Save credentials on successful sign-in.
+  authenticate(jwt, cb) {
+    if (typeof window !== "undefined") {
+      sessionStorage.setItem('jwt', JSON.stringify(jwt))
+    }
+    cb();
+  },
+//Delete credentials and sign out
+  signout(cb) {
+    if (typeof window !== "undefined") {
+      sessionStorage.removeItem('jwt')
+    }
+    cb();
+  }
+}
+
+export default auth;
