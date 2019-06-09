0c77b93a1d109e468baf745e5470d7ca4ebf282b
SMLukwiya <sundaymorganl@gmail.com>
2019-06-09 11:47:14 +0300

    server side configurations for variables

---
+++ config/config.js
@@ -0,0 +1,9 @@
+// Setup server side configurations for variables
+const config = {
+  env: process.env.NODE_ENV || 'development',
+  port: process.env.PORT || 3000,
+  jwtSecret: process.env.JWT_SECRET || 'Your_Secret_key',
+  mongoUri: process.env.MONGODB_URI || process.env.MONGO_HOST || 'mongodb://'+(process.env.IP||'localhost')+':'+(process.env.MONGO_PORT||'27017')+'/buyrent'
+}
+
+export default config;
