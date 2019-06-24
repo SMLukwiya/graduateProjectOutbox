5798fb3968b8dad03ca792a28d1c34f7630e114a
SMLukwiya <sundaymorganl@gmail.com>
2019-06-24 17:07:49 +0300

    pushing

--- config/config.js
+++ config/config.js
@@ -3,7 +3,7 @@ const config = {
   env: process.env.NODE_ENV || 'development',
   port: process.env.PORT || 3000,
   jwtSecret: process.env.JWT_SECRET || 'Your_Secret_key',
-  mongoUri: process.env.MONGODB_URI || process.env.MONGO_HOST || 'mongodb://'+(process.env.IP||'localhost')+':'+(process.env.MONGO_PORT||'27017')+'/buyrent'
+  mongoUri: process.env.MONGODB_URI || process.env.MONGO_HOST || 'mongodb+srv://SMLukiwya:anying@cluster0-mjnd7.gcp.mongodb.net/local_library?retryWrites=true'
 }
 
 export default config;
