74890bc02816ec39bec310109c037edc3dd9696f
SMLukwiya <sundaymorganl@gmail.com>
2019-06-24 13:15:58 +0300

    main router set up

--- client/MainRouter.js
+++ client/MainRouter.js
@@ -1,28 +1,22 @@
-import React, {Component} from 'react';
-import {Route} from 'react-router-dom';
-import {Switch} from 'react-router-dom';
+import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
 import Home from './Core/Home';
-import Users from './user/Users';
 import Signup from './user/Signup';
 import Signin from './auth/Signin';
-import Profile from './user/Profile';
-import EditProfile from './user/EditProfile';
-import PrivateRoute from './auth/PrivateRoute';
-import Menu from './core/Menu';
-import Buy from './houses/forSale/Buy'
-import Rent from './houses/forRent/Rent'
-import BuyDisplayHouse from './houses/forSale/DisplayHouse';
-import RentDisplayHouse from './houses/forRent/DisplayHouse';
+import Menu from './Core/Menu';
+import Buy from './houses/Buy'
+import Rent from './houses/Rent'
+import DisplayHouse from './houses/DisplayHouse';
+import Cart from './Cart/CartItems';
+import Checkout from './Cart/Checkout';
+import Order from './order/Order';
+import Footer from './Core/Footer';
+import CreateHouse from './houses/CreateHouse';
+import Favourite from './order/Favourite';
+import AdminPage from './admin/AdminPage';
+import AdminSignIn from './auth/AdminSignin';
 
 class MainRouter extends Component {
-  ///*** Server Side Rendering***///
-// Removes the server-side injected CSS when React component mounts
-  // componentDidMount() {
-  //   const jssStyles = document.querySelector('#jss-server-side');
-  //   if (jssStyles && jssStyles.parentNode) {
-  //     jssStyles.parentNode.removeChild(jssStyles);
-  //   }
-  // }
 
   render () {
     return (
@@ -34,12 +28,16 @@ class MainRouter extends Component {
           <Route path='/rent' component = {Rent} />
           <Route path='/signup' component = {Signup} />
           <Route path='/signin' component = {Signin} />
-          <Route path='/buyhouse/:houseId' component = {BuyDisplayHouse} />
-          <Route path='/renthouse/:houseId' component = {RentDisplayHouse} />
-          <PrivateRoute path='/user/edit/:userId' component = {EditProfile} />
-          //protected route, only accessible by authorized and authenticated users
-          <Route path='/user/:userId' component = {Profile} />
+          <Route path='/house/:houseId' component = {DisplayHouse} />
+          <Route path='/cart' component = {Cart} />
+          <Route path='/checkout' component = {Checkout} />
+          <Route path="/order/:orderId" component={Order}/>
+          <Route path="/create" component={CreateHouse}/>
+          <Route path="/favourite" component={Favourite}/>
+          <Route path='/access/admin' component = {AdminPage} />
+          <Route path='/adminsignin' component = {AdminSignIn} />
         </Switch>
+      <Footer />
       </div>
     );
   }
