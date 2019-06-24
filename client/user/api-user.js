29e4beccfc6554fe8b9e1b2c041772eb536cf145
SMLukwiya <sundaymorganl@gmail.com>
2019-06-24 13:11:32 +0300

    fetch api for user

--- client/user/api-user.js
+++ client/user/api-user.js
@@ -1,8 +1,8 @@
 // Fetch API to create user
 const create = (user) => {
-  return fetch('/api/users/', {
+  return fetch('/api/users', {
     method: 'POST',
-    header: {
+    headers: {
       'Accept': 'application/json',
       'Content-Type': 'application/json'
     },
@@ -16,7 +16,7 @@ const create = (user) => {
 
 // Fetch API to read a user
 const read = (params, credentials) => {
-  return fetch('/api/users/'+params.userId, {
+  return fetch('/api/user/'+params.userId, {
     method: 'GET',
     headers: {
       'Accept': 'application/json',
@@ -28,33 +28,5 @@ const read = (params, credentials) => {
   }).catch((err) => console.log(err));
 }
 
-// Fetch API to update a user
-const update = (params, credentials, users) => {
-  return fetch('/api/users/'+params.userId, {
-    method: 'PUT',
-    headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json',
-      'Authorization': 'Bearer '+ credentials.t
-    },
-    body: JSON.stringify(user)
-  }).then((response) => {
-    return response.json();
-  }).catch((err) => console.log(err));
-}
 
-// Fetch API to delete a user
-const remove = (params, credentials) => {
-  return fetch('/api/users/'+params.userId, {
-    method: 'DELETE',
-    headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json',
-      'Authorization': 'Bearer '+credentials.t
-    }
-  }).then((response) => {
-    return response.json();
-  }).catch((err) => console.log(err));
-}
-
-export { create, list, read, update, remove };
+export { create, read };
