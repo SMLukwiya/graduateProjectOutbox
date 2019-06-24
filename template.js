2a0c0876854f6a91363113cf215c532dd2c34dd6
SMLukwiya <sundaymorganl@gmail.com>
2019-06-24 09:19:38 +0300

    template file for client side rendering

--- template.js
+++ template.js
@@ -1,17 +1,21 @@
 export default () => {
-    return (`<!doctype html>
+    return `<!doctype html>
       <html lang="en">
         <head>
           <meta charset="utf-8">
           <title>BUYRENT</title>
           <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500&display=swap" />
           <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+          <style>
+          a{
+              text-decoration: none
+            }
+          </style>
           <style id="jss-server-side"></style>
         </head>
         <body>
           <div id="root"></div>
           <script type="text/javascript" src="/dist/bundle.js"></script>
-          <style id="jss-server-side"></style>
         </body>
-      </html>`)
+      </html>`
 }
